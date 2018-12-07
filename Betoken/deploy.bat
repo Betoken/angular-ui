026deb533fdbd0ef3ba1c51f27bb9d92ece637f7
Zefram Lou <zeframlou@gmail.com>
2018-12-07 14:33:09 -0800

    changed remote in deploy.bat

--- Betoken/deploy.bat
+++ Betoken/deploy.bat
@@ -1,2 +1,2 @@
 ng build --prod --base-href "https://betoken.fund/portal/"
-npx ngh --dir=dist/Betoken --repo=https://github.com/Betoken/portal.git --branch=master
+npx ngh --dir=dist/Betoken --repo=git@github.com:Betoken/portal.git --branch=master

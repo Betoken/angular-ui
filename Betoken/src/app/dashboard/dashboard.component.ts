b1eec7358f654a188be41b5b48f7235383a720a3
guillaume palayer <guillaume.palayer@gmail.com>
2019-01-17 15:46:36 -0200

    deleted: last change regarding userbalance

--- Betoken/src/app/dashboard/dashboard.component.ts
+++ Betoken/src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { } from 'jquery';
 declare var $: any;
 
 import {
-    user, timer, stats, investor_actions, manager_actions, tokens, refresh_actions
+    user, timer, stats, investor_actions, tokens
 } from '../../betokenjs/helpers';
 
 @Component({
@@ -31,7 +31,6 @@ import {
 })
 
 export class DashboardComponent implements OnInit {
-    userRanking = [];
     walkthrough: boolean;
     step1: boolean;
     step2: boolean;
@@ -61,10 +60,7 @@ export class DashboardComponent implements OnInit {
     inputShare = 0.00;
     calculated_balance = 0.00;
     selectedTokenSymbol = 'DAI';
-
-    kairo_balance = 0.0000;
-    monthly_pl = 0.00;
-    expected_commission = 0.00;
+    
     sharePrice = 0;
     avgMonthReturn = 0;
     currMoROI = 0;
@@ -334,12 +330,6 @@ export class DashboardComponent implements OnInit {
         this.totalUser = this.rankingArray.length;
     }
 
-    refreshDisplay() {
-      this.kairo_balance = user.portfolio_value().toFormat(10);
-      this.monthly_pl = user.monthly_roi().toFormat(4);
-      this.expected_commission = user.expected_commission().toFormat(4);
-      this.userRanking = user.rank();
-    }
 
     drawChart = () => {
         // Prepare data

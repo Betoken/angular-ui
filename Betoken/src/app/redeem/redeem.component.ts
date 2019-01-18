df990fed5d8844c2563a309cb25a9d7e46649ca4
Zefram Lou <zeframlou@gmail.com>
2019-01-17 19:02:31 -0800

    finished redeem commission

--- Betoken/src/app/redeem/redeem.component.ts
+++ Betoken/src/app/redeem/redeem.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { AppComponent } from '../app.component';
-import { user, timer, manager_actions, refresh_actions } from '../../betokenjs/helpers';
+import { user, manager_actions, refresh_actions } from '../../betokenjs/helpers';
+import { Router } from '@angular/router';
 
+
+declare var jquery:any;
+declare var $ :any;
 @Component({
     selector: 'app-redeem',
     templateUrl: './redeem.component.html',
@@ -20,27 +24,9 @@ import { user, timer, manager_actions, refresh_actions } from '../../betokenjs/h
 })
 export class RedeemComponent implements OnInit {
     redeemvalue: any;
-    redeemcommissionvalue: any;
-    selectedOption = 1;
-    tableData: any;
-    state: string;
-    active: boolean;
+    commissionAmount: any;
 
-    redeemalert: boolean;
-
-    step1: boolean;
-    step2: boolean;
-    step3: boolean;
-    step4: boolean;
-
-    footerbtn1: boolean;
-    footerbtn2: boolean;
-
-    days = 0;
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    phase = -1;
+    step: Number;
 
     transactionId: '';
 
@@ -48,121 +34,42 @@ export class RedeemComponent implements OnInit {
         this.ms.setredeemPopUp();
     }
 
-    constructor(private ms: AppComponent) {
-        this.state = 'open';
-        this.active = true;
-
-        this.redeemalert = true;
-
-        this.footerbtn1 = true;
-        this.footerbtn2 = false;
-
-        this.step1 = true;
-        this.step2 = false;
-        this.step3 = false;
-        this.step4 = false;
+    constructor(private ms: AppComponent, private router: Router) {
+        this.step = 0;
     }
 
     ngOnInit() {
         setInterval(() => {
-            this.updateDates();
-            this.redeemcommissionvalue = user.expected_commission().toFormat(18);
+            this.commissionAmount = user.expected_commission().toFormat(4);
         }, 100 );
-
-        this.ms.getredeemPopUp().subscribe((open: boolean) => {
-
-            if (open) {
-                this.state = 'open';
-                this.active = true;
-            }
-
-            if (!open) {
-                this.state = 'close';
-                this.active = false;
-            }
+        $('#modalRedeem').on('hidden.bs.modal', () => {
+            this.resetModals();
+            this.router.navigate(['/dashboard']);
         });
     }
 
-    async updateDates() {
-        this.days = timer.day();
-        this.hours = timer.hour();
-        this.minutes = timer.minute();
-        this.seconds = timer.second();
-        this.phase = timer.phase();
+    resetModals() {
+        this.step = 0;
     }
 
-    redeem() {
-        this.step2 = true;
-        this.step3 = false;
-        this.step4 = false;
-        this.step1 = false;
-        this.redeemCommission();
-    }
+    redeemCommission(option) {
+        this.step = 1;
 
-    pending = (transactionHash) => {
-        this.transactionId = transactionHash;
-        this.step1 = false;
-        this.step2 = false;
-        this.step3 = true;
-        this.step4 = false;
-    }
-
-    confirm = () => {
-        this.step1 = false;
-        this.step2 = false;
-        this.step3 = false;
-        this.step4 = true;
-        refresh_actions.records();
-    }
-
-    confirmredeem() {
-        this.step3 = true;
-        this.step4 = false;
-        this.step1 = false;
-        this.step2 = false;
-        setTimeout(() => {
-            this.step4 = true;
-            this.step1 = false;
-            this.step2 = false;
-            this.step3 = false;
-            this.redeemalert = false;
-            this.ms.setnewcyclebtn();
-            this.footerbtn2 = true;
-        }, 1000);
-    }
-
-    closePopup() {
-        this.state = 'close';
-        this.active = false;
-        this.step1 = true;
-        this.step2 = false;
-        this.step3 = false;
-        this.step4 = false;
-    }
-
-    hidealert() {
-        this.redeemalert = false;
-    }
-
-    redeemPopup() {
-        this.openredeemModal();
-    }
-
-    async redeemCommission() {
-        if (this.selectedOption === 1) {
-            this.redeemvalue = await manager_actions.redeem_commission(this.pending, this.confirm);
+        let pending = (transactionHash) => {
+            this.transactionId = transactionHash;
+            this.step = 2;
         }
-        if (this.selectedOption === 2) {
-            this.redeemvalue = await manager_actions.redeem_commission_in_shares(this.pending, this.confirm);
+    
+        let confirm = () => {
+            this.step = 3;
+            refresh_actions.records();
         }
-    }
 
-    updateRedeemOption(event) {
-        const value = event.target.value.trim();
-        if (value === 'DAI Stablecoin') {
-            this.selectedOption = 1;
-        } else {
-            this.selectedOption = 2;
+        if (option === 0) {
+            manager_actions.redeem_commission(pending, confirm);
+        }
+        if (option === 1) {
+            manager_actions.redeem_commission_in_shares(pending, confirm);
         }
     }
 }

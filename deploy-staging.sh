8d4ba05353c67dcf3c6d1a0ffe1b7502b4b63db4
Zefram Lou <zeframlou@gmail.com>
2019-09-09 14:17:55 +0800

    added deployment script for deploying multi-language version

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,1 +1,5 @@
-ng deploy --repo=git@github.com:Betoken/portal-staging.git --branch=master --base-href=https://betoken.fund/portal-staging/
+ng build --prod --base-href=/portal-staging/
+ng build --configuration=ja --base-href=/portal-staging/ja/
+ng build --configuration=pt --base-href=/portal-staging/pt/
+
+ng deploy --repo=git@github.com:Betoken/portal-staging.git --branch=master --no-build

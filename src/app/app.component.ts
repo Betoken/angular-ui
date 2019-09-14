e51a50d6fda8f428badb8edb819e5bb172321cc1
Zefram Lou <zeframlou@gmail.com>
2019-09-15 01:10:29 +0800

    added translation for meta tags & title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Injectable, Inject } from '@angular/core';
 import { Betoken } from '../betokenjs/betoken-obj';
 import { loadAllData, loadDynamicData } from '../betokenjs/data-controller';
 import { LOCALE_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Injectable()
 
@@ -10,12 +11,11 @@ import { LOCALE_ID } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Betoken';
   display_header_sidebar = true;
   is_loading = true;
   load_progress = 20;
 
-  constructor(@Inject(LOCALE_ID) public locale: string) {
+  constructor(@Inject(LOCALE_ID) private locale: string, private meta: Meta, private title: Title) {
     const betoken = new Betoken();
     betoken.init().then(() => {
       this.load_progress = 50;
@@ -27,6 +27,12 @@ export class AppComponent {
       setInterval(loadDynamicData, 120 * 1000); // refresh everything every 2 minutes
       this.is_loading = false;
     });
+
+    let metaTags = require(`../locale/meta.${locale}.json`);
+    this.meta.addTags(metaTags);
+
+    let titleText = require(`../locale/title.${locale}.json`);
+    this.title.setTitle(titleText);
   }
 
   loadProgressPercentage() {

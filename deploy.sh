9ac38b7c7d30c5b940d3d3b7cfb1c90034a14144
Zefram Lou <zeframlou@gmail.com>
2019-09-18 15:29:56 +0800

    added production deployment script, improved router links

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-ng deploy --repo=git@github.com:Betoken/portal.git --branch=master --base-href=https://betoken.fund/portal/
+ng build --prod --base-href=/portal/
+ng build --configuration=ja --base-href=/portal/ja/
+ng build --configuration=pt --base-href=/portal/pt/
+ng build --configuration=zh-Hans --base-href=/portal/zh-Hans/
+
+node addMetaTags.js
+
+ng deploy --repo=git@github.com:Betoken/portal.git --branch=master --no-build
